3cff6ba3ee3376bca36b6649ff33bfe891b07962
rongfl <1141591465@qq.com>
2018-01-06 11:57:58 +0800

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# es6 node run demo 
+
+##  init project
+```bash
+yarn
+```
+## run demo
+```bash
+yarn run run
+```
---
+++ main.js
@@ -0,0 +1,4 @@
+import  user from "./user.js";
+let foo = (item) =>console.log(JSON.stringify(item));
+foo(user.userinfo);
+foo(user.moduleinfo);
